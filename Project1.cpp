c621550b62c2701c8761dadfbc95e8299c74fc20
bkeeley3 <98189772+bkeeley3@users.noreply.github.com>
2022-01-26 16:04:55 -0800

    Add files via upload

--- Project1.cpp
+++ Project1.cpp
@@ -1,9 +1,9 @@
 /*
-  Brady Keeley
-  CPSC 122
+  Name: Brady Keeley
+  Class: CPSC 122 Section 
   Date: 1/21/2022
-  Project 1
-  Prints a specified number if prime numbers in a specific number of coloums determined by the user
+  Assignment: Project 1
+  Description: Prints a specified number if prime numbers in a specific number of coloums determined by the user
 */
 
 #include <iostream>
@@ -15,14 +15,18 @@ void error(int);
 
 int main(int argc, char*argv[])
 {
- int nums, cols;
+  int nums, cols;
 
- nums = atoi(argv[1]); ///retrieves the amount of numbers to be displayed from the command line
- cols = atoi(argv[2]); ///retrieves the amount of coloums to display the numbers across from the command line
+  nums = atoi(argv[1]); ///retrieves the amount of numbers to be displayed from the command line
+  cols = atoi(argv[2]); ///retrieves the amount of coloums to display the numbers across from the command line
+  if (nums < 1)
+    error(1);
+  if (cols < 1)
+    error(2);
 
- display(nums,cols);
- cout << endl;
- return 0;
+  display(nums,cols);
+  cout << endl;
+  return 0;
 } 
 
 /*
@@ -33,24 +37,24 @@ int main(int argc, char*argv[])
 */
 void display(int totalPrimes, int cols)
 {
- int ct = 1;
- int value = 0; 
- while (ct <= totalPrimes)
- {
-  if (isPrime(value))
+  int ct = 1;
+  int value = 0; 
+  while (ct <= totalPrimes)
   {
-  cout << value << '\t';
-  //if (ct % cols == cols - 1)  //start counting from 0
-  if (ct % cols == 0)  //start counting from 1
-   cout << endl;
-  ct++;
-  value++;
+    if (isPrime(value))
+    {
+      cout << value << '\t';
+      //if (ct % cols == cols - 1)  //start counting from 0
+      if (ct % cols == 0)  //start counting from 1
+      cout << endl;
+      ct++;
+      value++;
+    }
+    else
+    {
+      value++;
+    }
   }
-  else
-  {
-    value++;
-  }
- }
 }
  
 /*
@@ -60,58 +64,62 @@ void display(int totalPrimes, int cols)
 */
 bool isPrime(int num)
 {
- bool prime = false;
+  bool prime = false;
  
- if (num % 2 == 0)///if else structure to sort and assign prime or not prime 
- {
-   if (num == 2) /// hard code checking for single didget primes in each if statement
-    prime = true;
-   else
+  if (num % 2 == 0)///if else structure to sort and assign prime or not prime 
+  {
+    if (num == 2) /// hard code checking for single didget primes in each if statement
+      prime = true;
+    else
+      prime = false;
+  }
+  else if (num % 3 == 0)
+  {
+    if (num == 3)
+      prime = true;
+    else
+      prime = false;
+  }
+  else if (num % 5 == 0)
+  {
+    if (num == 5)
+      prime = true;
+    else
+      prime = false;
+  }
+  else if (num % 7 == 0)
+  {
+    if (num == 7)
+      prime = true;
+    else
+      prime = false;
+  }
+  else if (num % 9 == 0)
+  {
     prime = false;
- }
- else if (num % 3 == 0)
- {
-   if (num == 3)
+  }      
+  else
+  {
     prime = true;
-   else
-    prime = false;
- }
- else if (num % 5 == 0)
- {
-   if (num == 5)
-    prime = true;
-   else
-    prime = false;
- }
- else if (num % 7 == 0)
- {
-   if (num == 7)
-    prime = true;
-   else
-    prime = false;
- }
- else if (num % 9 == 0)
- {
-   prime = false;
- }      
- else
- {
-   prime = true;
- }
- return prime;
+  }
+  return prime;
 }
 
-
-void error(int code) ///checks to make sure inputs are integers that are greater than or equal to 1
+/*
+  checks to make sure inputs are integers that are greater than or equal to 1
+  Input: int that pertains to a certian case, 1 for numbers, and 2 for columns
+  Output: if error is found it prints to the user what was wrong
+*/
+void error(int code) 
 {
- if (code == 1)
+  if (code == 1)
   {
-   cout <<  "Numbers displayed must be >= 1" << endl;
-   exit(EXIT_FAILURE);
+    cout <<  "Numbers displayed must be >= 1" << endl;
+    exit(EXIT_FAILURE);
   }
- if (code == 2)
+  if (code == 2)
   {
-   cout <<  "Columns must be >= 1" << endl;
-   exit(EXIT_FAILURE);
+    cout <<  "Columns must be >= 1" << endl;
+    exit(EXIT_FAILURE);
   }
 }
